11f065811eacb8961f9c66e5e1de70289e142b20
tbhachu <sem.b1901@gmail.com>
2023-01-22 17:15:11 -0800

    pushed changes

---
+++ views/index.php
@@ -0,0 +1,24 @@
+    <?php
+      include 'header.php';
+    ?>
+  
+  
+    <div class="container">
+      <div class="container text-center">
+        <h1>Advise-It</h1>
+      </div>
+      <br>
+      <div class="container text-center">
+        <form action="planPage.php" method="post">
+          <button>Create New Plan</button>
+        </form>
+      </div>
+
+    </div>
+    <?php
+      include 'footer.php';
+    ?>
+
+    
+
+
---
+++ views/planPage.php
@@ -0,0 +1,45 @@
+    <?php
+      include 'header.php';
+    ?>
+    
+    <!-- Token -->
+    <div class="container text-center">
+      <h1>Quarterly Plan</h1><hr>
+      
+      <div class="container text-center">
+        <p>Token: </p>
+        <div id='token'>
+          <!-- This div will display our generated token -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Four quarters representing Fall, Winter, Spring and Summer -->
+    <div class="container text-center">
+      <div class="row">
+        <div class="card">Fall<hr>
+          <div >
+            <textarea type="text"></textarea>
+          </div>
+        </div>  
+        <div class="card">Winter<hr>
+          <div>
+            <textarea type="text"></textarea>
+          </div>
+        </div>
+        <div class="card">Spring<hr>
+          <div class="center">
+            <textarea type="text"></textarea>
+          </div>
+        </div>
+        <div class="card">Summer<hr>
+          <div class="center">
+            <textarea type="text"></textarea>
+          </div>
+        </div>
+    </div>
+    
+    
+    <?php
+      include 'footer.php';
+    ?>
